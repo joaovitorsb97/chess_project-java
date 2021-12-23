fd7d5ed1face8b3aaf9219712bf6c2cfb0a84b4e
Joao Vitor Bastos <jvs.bastos@gmail.com>
2021-12-22 23:44:13 -0300

    First class: Position

--- src/applicattion/Program.java
+++ src/applicattion/Program.java
@@ -1,11 +1,15 @@
 package applicattion;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 
 		
-		System.out.println("Hello World");
+		Position pos = new Position(3, 5);
+		
+		System.out.println(pos);
 	}
 
 }
