d3d777d7ce00474609047bbd094f902b4d17f437
Joao Vitor Bastos <jvs.bastos@gmail.com>
2021-12-26 21:42:07 -0300

    BoardException and defensive programming

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -7,7 +7,9 @@ public class Board {
 	private Piece[][] pieces;
 	
 	public Board(int rows, int columns) {
-		
+		if(rows < 1 || columns < 1) {
+			throw new BoardException("Error! There must be at least 1 row and 1 column in the board");
+		}
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
@@ -34,13 +36,35 @@ public class Board {
 	}
 	
 	public Piece piece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board!");
+		}
 		return pieces[position.getRow()][position.getColumn()];
 		
 	}
 	
 	public void placePiece(Piece piece, Position position) {
+		if(thereIsAPiece(position)) {
+			throw new BoardException("There is already a piece on position");
+		}
+		
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
 		
+	}
+	
+	private boolean positionExists(int row, int column) {
+		return row >= 0 && row < rows && column >= 0 && column < columns;
+ 	}
+	
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+	}
+	
+	public boolean thereIsAPiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not exists!");
+		}
+		return piece(position) != null;
 	}
 }
